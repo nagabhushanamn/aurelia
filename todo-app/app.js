8f05439411562c34e9e9d00244272637d8a082df
Nagabhushanam <nagabhushanamn@gmail.com>
2018-03-24 18:15:27 +0530

    array methods

--- todo-app/app.js
+++ todo-app/app.js
@@ -3,11 +3,14 @@
 // step-1 : create Todo model class ( ES5 | ES6 )
 class Todo {
     constructor(title) {
-        this.id = 0;
+        Todo.id++;
+        this.id = Todo.id;
         this.title = title;
         this.completed = false;
     }
 }
+Todo.id = 0;
+
 // --------------------------------------------------
 // step-2 : create Todo service 
 class TodoService {
@@ -16,10 +19,34 @@ class TodoService {
     }
     addTodo(title) {
         let newTodo = new Todo(title);
-        this.todos.push(newTodo);
+        this.todos = [...this.todos, newTodo];
     }
-    // ...
+    editTodo(id, newTitle) {
+        // this.todos = this.todos.map(function (todo) {
+        //     if (todo.id !== id) return todo;
+        //     else Object.assign({}, todo, { title: newTitle });
+        // });
+        this.todos = this.todos.map(todo => (todo.id !== id) ? todo : Object.assign({}, todo, { title: newTitle }));
+    }
+    completeTodo(id) {
+        //..
+    }
+    completeAll() {
+        //..
+    }
+    deleteTodo(id) {
+        //
+    }
+    clearCompleted() {
+        //.
+    }
+    viewAll(status) {
+        if (status === 'all') {
+            this.todos.forEach(todo => console.log(todo));
+        }
+    }
 }
 // --------------------------------------------------
 
 // step-3 : show demo in console
+let service = new TodoService();
